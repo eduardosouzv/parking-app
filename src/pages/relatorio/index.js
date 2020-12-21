e6cf034e889915eb89a0feeb32913e036fe22365
Eduardo Souza <eduardosouzv@gmail.com>
2020-12-21 01:57:11 -0300

    mostrando dados relatorio

--- src/pages/relatorio/index.js
+++ src/pages/relatorio/index.js
@@ -3,16 +3,30 @@ import React from 'react'
 import LineRelatorio from '../../components/linerelatorio'
 
 const Relatorio = () =>{
+
+    
+    function showData() {
+        var carrosJSONstring = localStorage.getItem('carros')
+        var carros = JSON.parse(carrosJSONstring);
+        var elements = []
+
+        for (let i = 0; i < carros.length; i++) {
+            
+            if(carros[i].valor_pago) {
+                elements.push(<LineRelatorio key={i.toString()} placa={ carros[i].placa } marca={ carros[i].marca } modelo={ carros[i].modelo } valor={ carros[i].valor_pago } />)
+            }
+
+        }
+        return elements;
+    }
+
     return (
         <div style={{ textAlign: "center" }} className="container">
 
             <ul className="list-group-item">
             <h1 className="h1">Relatorio do Dia</h1>
-                <LineRelatorio placa="AAA7777" marca="HONDA" modelo="CIVIC" valor="10.00" />
-                <LineRelatorio placa="AAA7777" marca="HONDA" modelo="CIVIC" valor="10.00" />
-                <LineRelatorio placa="AAA7777" marca="HONDA" modelo="CIVIC" valor="10.00" />
-                <LineRelatorio placa="AAA7777" marca="HONDA" modelo="CIVIC" valor="10.00" />
-                <LineRelatorio placa="AAA7777" marca="HONDA" modelo="CIVIC" valor="10.00" />
+            {showData()}
+
             </ul>
 
         </div>
