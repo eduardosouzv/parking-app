b8c284542907d2a0d32b6a79469f16358bf005d4
Eduardo Souza <eduardosouzv@gmail.com>
2020-12-21 17:18:06 -0300

    arrumando style relatorio

--- src/components/linerelatorio.js
+++ src/components/linerelatorio.js
@@ -3,11 +3,13 @@ import React from 'react';
 const LineRelatorio = (props) =>{
     return (
         <div className="">
-            <li className="list-group-item row col-md-12"><h5>
-                Placa : {props.placa} | 
-                Marca/Modelo : {props.marca}, {props.modelo} |
-                Valor Pago : R$ {props.valor}
-                </h5></li>
+            <li className="list-group-item row col-md-12">
+                <h5>
+                <span className="font-weight-bold">Placa</span> : <span className="font-weight-light">{props.placa} </span> | 
+                <span className="font-weight-bold">Marca/Modelo</span> : <span className="font-weight-light">{props.marca}, {props.modelo}</span> |
+                <span className="font-weight-bold">Valor Pago</span> : <span className="font-weight-light">R$ {props.valor},00</span>
+                </h5>
+            </li>
         </div>
     );
 }
