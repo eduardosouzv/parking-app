ff8b962fac5b895dd0394ad18a8bb6105ec85d71
Eduardo Souza <eduardosouzv@gmail.com>
2021-04-03 15:00:58 -0300

    using hook

--- frontend/src/pages/relatorio.js
+++ frontend/src/pages/relatorio.js
@@ -1,46 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-import LineRelatorio from '../components/linerelatorio'
+import LineRelatorio from '../components/linerelatorio';
 
-const Relatorio = () =>{
-    window.onload = showData();
+const Relatorio = () => {
+  useEffect(() => {
+    loadCurrentReport();
+  }, []);
 
-    async function showData() {
-        const cars = await axios.get('http://localhost:3001/api/relatorio');
-        var elements = [];
-    
-        if (!cars) {
-          return;
-        } else {
-          for (let i = 0; i < cars.data.length; i++) {
-            const el = React.createElement(
-                LineRelatorio,
-              {
-                key: i.toString(),
-                placa: cars.data[i].placa,
-                marca: cars.data[i].marca,
-                modelo: cars.data[i].modelo,
-                valor: cars.data[i].valor_pago,
-              }
-            );
-            elements.push(el);
-          }
-          ReactDOM.render(elements, document.getElementById('cars'));
-        }
+  async function loadCurrentReport() {
+    const cars = await axios.get('http://localhost:3001/api/relatorio');
+    let elements = [];
+
+    if (!cars) {
+      return;
+    } else {
+      for (let i = 0; i < cars.data.length; i++) {
+        const el = React.createElement(LineRelatorio, {
+          key: i.toString(),
+          placa: cars.data[i].placa,
+          marca: cars.data[i].marca,
+          modelo: cars.data[i].modelo,
+          valor: cars.data[i].valor_pago,
+        });
+        elements.push(el);
       }
+      ReactDOM.render(elements, document.getElementById('cars'));
+    }
+  }
 
-    return (
-        <div style={{ textAlign: "center" }} className="container">
-
-            <ul className="list-group-item">
-            <h1 className="h1">Relatorio do Dia</h1>
-            <div id="cars"></div>
-            </ul>
-
-        </div>
-    );
-}
+  return (
+    <div style={{ textAlign: 'center' }} className="container">
+      <ul className="list-group-item">
+        <h1 className="h1">Relatorio do Dia</h1>
+        <div id="cars"></div>
+      </ul>
+    </div>
+  );
+};
 
 export default Relatorio;
